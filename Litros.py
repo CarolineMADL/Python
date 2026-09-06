017572bf71222586bb176f38816021a60468fe35
CarolineMADL <caroline.lima15@aluno.cps.sp.gov.br>
2026-09-06 11:59:54 -0300

    Add files via upload

--- Litros.py
+++ Litros.py
@@ -1,7 +1,12 @@
 #Litros
-#Tempo= T Velocidade = V Distancia = D Litros=L
-T = 2
-V = 20
-D = T*V
+#Tempo= T Velocidade média = VM Distancia = D Litros=L
+
+T = float(input("Digite o tempo gasto na viagem em horas: "))
+
+VM= float(input("Digite a velocidade média em km/h: "))
+
+D = T*VM
+
 L = D/12
+
 print("A quantidade de litros de combustível gastos na viagem é: ", L)
